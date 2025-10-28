ae4c48cd5aa9816a1d082f2d784530d4e3703891
Olamide Omolola <omololaolamidex@gmail.com>
2025-10-28 01:06:24 +0100

    uploaded module 5 and hw 5

---
+++ hw5/predict_service.py
@@ -0,0 +1,16 @@
+import pickle
+from fastapi import FastAPI
+import uvicorn
+from typing import Dict, Any
+
+app = FastAPI(title="lead-scoring-prediction")
+
+with open('pipeline_v1.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    return pipeline.predict_proba(customer)[0, 1]
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
